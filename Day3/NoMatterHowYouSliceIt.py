ac8109f915a79531875f2cc1da339d8c76331e9f
Lucile Madoulaud <madoulaud.lucile@gmail.com>
2018-12-03 19:16:55 +0100

    Day3 Part One

---
+++ Day3/NoMatterHowYouSliceIt.py
@@ -0,0 +1,24 @@
+# Advent of Code 2018 
+# --- Day 3: No Matter How You Slice It ---
+
+grid = {}
+count = 0
+lines = open('day3.txt').readlines()
+for line in lines:
+	claimID, claimInfos = line.strip().split(" @ ")
+	print(claimID)
+	coords, dim = claimInfos.split(": ")
+	x, y = [int(n) for n in coords.split(",")]
+	w, h = [int(k) for k in dim.split("x")]
+	for i in range(x, x+w):
+		for j in range(y, y+h):
+			if (i,j) not in grid:
+				grid[(i,j)]=[]
+			grid[(i,j)].append(claimID)
+
+for coord, data in grid.items():
+	if len(data) > 1:
+		count+=1
+
+print(count)
+print(len([data for _, data in grid.items() if len(data) > 1]))
