06e91e57de257e9e7cb46c5c687006d1b9b62df4
Lucile Madoulaud <madoulaud.lucile@gmail.com>
2018-12-03 19:42:49 +0100

    Day 3 - Part Two

--- Day3/NoMatterHowYouSliceIt.py
+++ Day3/NoMatterHowYouSliceIt.py
@@ -1,12 +1,13 @@
 # Advent of Code 2018 
 # --- Day 3: No Matter How You Slice It ---
 
+allClaimsID=[]
 grid = {}
 count = 0
 lines = open('day3.txt').readlines()
 for line in lines:
 	claimID, claimInfos = line.strip().split(" @ ")
-	print(claimID)
+	allClaimsID.append(claimID)
 	coords, dim = claimInfos.split(": ")
 	x, y = [int(n) for n in coords.split(",")]
 	w, h = [int(k) for k in dim.split("x")]
@@ -22,3 +23,10 @@ for coord, data in grid.items():
 
 print(count)
 print(len([data for _, data in grid.items() if len(data) > 1]))
+
+# --- Part Two ---
+setClaim = set(allClaimsID)
+for claims in grid.values():
+	if len(claims)>=2:
+		setClaim=setClaim.difference(claims)
+print(setClaim)
