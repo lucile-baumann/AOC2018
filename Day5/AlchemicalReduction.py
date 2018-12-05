e9b80841ece0ad634b210db1bcd93e6a6c45887f
Lucile Madoulaud <L.Madoulaud@avo-adam.com>
2018-12-05 12:46:28 +0100

    Add day5 part two

--- Day5/AlchemicalReduction.py
+++ Day5/AlchemicalReduction.py
@@ -2,6 +2,8 @@
 # --- Day 5: Alchemical Reduction ---
 
 data = open('day5.txt').read().strip()
+
+data1 = data
 i=0
 while i<len(data)-1:
 	#ord return the unicode code of a char 
@@ -16,3 +18,23 @@ while i<len(data)-1:
 		i+=1
 
 print(len(data))
+
+# --- Part Two ---
+minimum = 9999999999
+for letter in "abcdefghijklmnopqrstuvxyz":
+	infos = data1
+	test = infos.replace(letter,"").replace(letter.upper(),"")
+	i=0
+	while i<len(test)-1:
+		if abs(ord(test[i])-ord(test[i+1]))==32:
+			if i==len(test)-1:
+				test=test[:i]
+			else:
+				test=test[:i]+test[i+2:]
+				i = max(0, i-1)
+		else:
+			i+=1
+	result = len(test)
+	print(result)
+	minimum = min(minimum, result)
+print(minimum)
